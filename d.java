f0f58c7f841a38e26e9b0378468a87164bcfe88d
Chirasmit123 <nandachirasmita46@gmail.com>
2021-02-27 13:00:19 +0530

    Java program

---
+++ d.java
@@ -0,0 +1,15 @@
+public class d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{ 
+			int i=1;
+			do {
+			System.out.println("Hello World");
+			
+			i++;}
+			while (i<=10);
+		}
+	}
+
+}
